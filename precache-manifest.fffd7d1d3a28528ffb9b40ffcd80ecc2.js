5826e09c1e5608514dc5c0e7e64e532f4a94c290
Jakob Jarosch <dev@jakobjarosch.de>
2019-04-16 21:59:38 +0200

    Updates

---
+++ precache-manifest.fffd7d1d3a28528ffb9b40ffcd80ecc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "f9006f2de6321e7907cd",
+    "url": "./static/js/main.9f5132a5.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "./static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "c7ae01129ca66214597bade596efbe11",
+    "url": "./index.html"
+  }
+];
